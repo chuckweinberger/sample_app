e69358431972c2f52afe7b9427f2e46eec33ce2b
Chuck Weinberger <chuckhw@sbcglobal.net>
2010-10-15 14:38:08 -0700

    finished Exercise 5.5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Return the logo
+  def logo
+	return image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
 end
