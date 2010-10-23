e3ca257bce4333809f46372eb64c69fe61b04cd9
Chuck Weinberger <chuckhw@sbcglobal.net>
2010-10-23 12:31:46 -0700

    Made changes in Exercise Section of Chapter 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,16 +36,39 @@ describe UsersController do
 
   end
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
+	describe "GET 'new'" do
+	
+
+
+		it "should be successful" do
+			get 'new'
+			response.should be_success
+		end
     
-    it "should have the right title" do
-      get 'new'
-      response.should have_selector("title", :content => "Sign up")
-    end
+		it "should have the right title" do
+			get 'new'
+			response.should have_selector("title", :content => "Sign up")
+		end
+		
+		it "should have a name field" do
+			get :new
+			response.should have_selector("input[name='user[name]'][type='text']")
+		end
+
+		it "should have an email field" do
+			get :new
+			response.should have_selector("input[name='user[email]'][type='text']")
+		end
+		
+		it "should have a password field" do
+			get :new
+			response.should have_selector("input[name='user[password]'][type='password']")
+		end
+		
+		it "should have a password confirmation field" do
+			get :new
+			response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+		end	
   end
   
   describe "POST 'create'" do
