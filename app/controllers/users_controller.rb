eb4f56cb93c945d8e7479233b977233e74ecb7c6
Chuck Weinberger <chuckhw@sbcglobal.net>
2010-11-23 17:31:17 -0800

    done with user edit/update, index, and destroy actions:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,71 @@
 class UsersController < ApplicationController
   
-  def show
-    @user = User.find(params[:id])
-	@title = @user.name
-  end
-
-  def new
-    @title = "Sign up"
-	@user = User.new
+  before_filter :authenticate, 	:only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, 	:only => [:edit, :update]
+  before_filter :admin_user, 	:only => :destroy
+  
+  	
+	def index
+		@title = "All users"
+		@users = User.paginate(:page => params[:page])
+	end
+	
+	def show
+		@user = User.find(params[:id])
+		@title = @user.name
 	end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-		sign_in @user
-		flash[:success] = "Welcome to the Sample App!"
-		redirect_to @user
-    else
+	def new
 		@title = "Sign up"
-		@user.password = ""
-		@user.password_confirmation = ""
-		render 'new'
-    end
-  end
+		@user = User.new
+	end
 
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			sign_in @user
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			@title = "Sign up"
+			@user.password = ""
+			@user.password_confirmation = ""
+			render 'new'
+		end
+	end
+ 
+	def edit
+		@title = "Edit user"
+	end
+
+    def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated."
+			redirect_to @user
+		else
+			@title = "Edit user"
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed"
+		redirect_to users_path
+	end
+	
+	private
+		def authenticate
+			deny_access unless signed_in?
+		end
+		
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def	admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
 end
